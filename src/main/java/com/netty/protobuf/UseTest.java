07008cac082f49c3abe699055b1cb5982769f0e8
YCLlong <251121753@qq.com>
2018-11-27 22:54:13 +0800

    netty 实现 webSocket服务器 和前端 JS 客户端通信

---
+++ src/main/java/com/netty/protobuf/UseTest.java
@@ -0,0 +1,39 @@
+package com.netty.protobuf;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.netty.protobuf.proto.DataInfo;
+
+import java.util.Arrays;
+
+public class UseTest {
+    public static void main(String[] args) {
+    //将student对象序列化成字节数组
+        DataInfo.Student student = DataInfo.Student.newBuilder()
+                .setName("燕成龙")
+                .setAge(22)
+                .setAddress("安徽省 安庆市")
+                .build();
+        byte[] studentByteArr = student.toByteArray();
+        //测试加一个字节是否会序列化失败
+        byte[] studebtCopy = new byte[studentByteArr.length + 1];
+        studebtCopy = Arrays.copyOf(studentByteArr,studebtCopy.length);
+        studebtCopy[studentByteArr.length]=10;
+        for(byte bt : studebtCopy){
+            System.out.print(bt + " ");
+        }
+        System.out.println();
+        DataInfo.Student student1 = null;
+        try {
+            //将字节数组生成对象，这里用测试的字节数组，会抛出异常
+            student1 = DataInfo.Student.parseFrom(studebtCopy);
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        }
+
+        if(student1 != null){
+            System.out.println(student1.getName());
+            System.out.println(student1.getAge());
+            System.out.println(student1.getAddress());
+        }
+    }
+}
